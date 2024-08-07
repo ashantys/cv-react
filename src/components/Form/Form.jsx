22d5917d60118958bd09c94729377b3fc61208db
ashantys <100235782+ashantys@users.noreply.github.com>
2024-08-07 13:02:23 -0600

    feat: subiendo el form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,25 +1,46 @@
-import React from "react";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 
 export default function Form () {
-    return(
-        <form>
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" />
-        </div>
-        <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-          <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
-    );
 
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, {
+        publicKey: 'YOUR_PUBLIC_KEY',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+    }
+    return(
+    <div className="p-5 row bg-white rounded-4">
+      <h1 className="p-2">Contact Me</h1>
+        <form ref={form} onSubmit={sendEmail}>
+        <div class="mb-3">
+          <label>Name</label>
+          <input class="form-control" type="text" name="user_name" />
+        </div>
+        <div class="mb-3">
+          <label>Email</label>
+          <input class="form-control" type="email" name="user_email" />
+        </div>
+        <div class="mb-3">
+        <label>Message</label>
+          <textarea class="form-control" name="message" />
+        </div>
+        <button type="submit" class="btn btn-dark">Submit</button>
+        </form>
+    </div>
+    );
 
 }
